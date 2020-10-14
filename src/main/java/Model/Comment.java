2d37bb7f8681f61b5646d41a6a4f8bbd5c03e4b4
jakub.skibinski <jakub.skibinski@sonalake.com>
2020-10-14 14:08:27 +0200

    refactoring

---
+++ src/main/java/Model/Comment.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Comment {
+
+    String author;
+    String commentMessage;
+    String email;
+    String website;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCommentMessage() {
+        return commentMessage;
+    }
+
+    public void setCommentMessage(String commentMessage) {
+        this.commentMessage = commentMessage;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+}

e8990f644344d99ec0b9b862442ddfd0a17ec735
jakub.skibinski <jakub.skibinski@sonalake.com>
2020-10-14 14:13:55 +0200

    refactoring

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/demo.feature"},
+        features = {"src/test/resources/"},
         plugin = { "pretty", "html:target/htmlreports" }
 )
+
 public class TestRunner {
 
 }
